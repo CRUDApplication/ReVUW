abd9ce19e2ced4d181d57974f6f75bdb102add18
Samuel Richards <richards.m.samuel@gmail.com>
2023-09-22 21:42:37 +1200

    feat: #7 added routes and middleware to server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,48 @@
+require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const path = require('path');
+const passportSetup = require(path.join(__dirname, 'utils', 'passport-setup'));
+
 const app = express();
 
+// Set up view engine and views directory
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.set('views', __dirname + '/views');
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
+// Parse incoming request bodies (for form data)
+app.use(express.urlencoded({ extended: false }));
 
+// Set up sessions
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Initialize Passport and session for Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes
+app.use('/auth', require('./routes/auth'));
+
+// Ensure authenticated middleware for protected routes
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/login');
+}
+
+// Root route
 app.get('/', (req, res) => {
     res.render('index', { title: 'ReVUW' });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-   console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
