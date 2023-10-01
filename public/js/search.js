93c9d662b160af4f4b8cd473c887831979c3768a
“Firstname <batoondian@ecs.vuw.ac.nz>
2023-10-01 21:51:03 +1300

    Add a search bar for courses

---
+++ public/js/search.js
@@ -0,0 +1,20 @@
+// Display the courses in the list that match the input search term
+function initSearch(courseList) {
+    const courseSearchInput = document.getElementById('courseSearch');
+    const courses = [...courseList.querySelectorAll('li')];
+
+    courseSearchInput.addEventListener('input', () => {
+        const searchTerm = courseSearchInput.value.toLowerCase();
+
+        courses.forEach(course => {
+            const title = course.querySelector('.course-title').textContent.toLowerCase();
+            const courseCode = course.querySelector('.course-code').textContent.toLowerCase();
+            
+            if (title.includes(searchTerm) || courseCode.includes(searchTerm)) {
+                course.style.display = 'block';
+            } else {
+                course.style.display = 'none';
+            }
+        });
+    });
+}
