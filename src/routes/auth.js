5f93fd41c7789a7ec60f1b767e53bd7c7d3270b0
Samuel Richards <richards.m.samuel@gmail.com>
2023-10-03 19:05:03 +1300

    fix: #23 prevented duplicate email signups in auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -11,14 +11,20 @@ router.get('/signup', (req, res) => {
 });
 
 router.post('/signup', async (req, res) => {
-  let userPassword = req.body.registerPassword;
+  const userPassword = req.body.registerPassword;
+  const userEmail = req.body.email;
   let passwordCheckResult = checkPasswordStrength(userPassword);
   if (passwordCheckResult != null) {
     res.render('signin', { title: 'ReVUW | SignUp', user: req.session.user, userEmail: req.body.email, errorMessage: passwordCheckResult, activeTab: 'register'});
   } 
   else {
     try {
-      await User.create({ email: req.body.email, password: userPassword });
+      const emailIsUnique = await isEmailUnique(userEmail);
+      if (!emailIsUnique) {
+        res.render('signin', { title: 'ReVUW | SignUp', user: req.session.user, userEmail: req.body.email, errorMessage: 'Email already in use', activeTab: 'register'});
+      }
+
+      await User.create({ email: userEmail, password: userPassword });
       res.redirect('/');
     } catch (error) {
       res.render('signin', { errorMessage: error.message, title: 'Authentication Failed', user: req.session.user, activeTab: 'register' });
@@ -36,16 +42,29 @@ function checkPasswordStrength(userPassword) {
   return errorMessage;
 }
 
+
+async function isEmailUnique(email) {
+  try {
+    const existingUser = await User.findOne({ email: email });
+    return existingUser == null; // Return true if email is unique
+  } catch (error) {
+    console.log('Error while checking email uniqueness:', error);
+    throw error; 
+  }
+}
+
 router.get('/signin', (req, res) => {
   const errorMessage = req.flash('error');
   res.render('signin', { errorMessage: errorMessage[0], title: 'ReVUW | Login', user: req.session.user, activeTab: 'login' });
 });
 
 router.post('/signin', passport.authenticate('local', {
-  successRedirect: '/',
   failureRedirect: '/auth/signin',
   failureFlash: true
-}));
+}), (req, res) => {
+  const redirectURL = req.query.origin || '/'; // Default redirect URL
+  res.redirect(redirectURL);
+});
 
 router.post('/logout', function(req, res, next) {
   req.logout(function(err) {
