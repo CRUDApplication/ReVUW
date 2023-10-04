f7df874b9b58c1efaea2c6f862eb00b622d44c34
Samuel Richards <richards.m.samuel@gmail.com>
2023-10-04 20:51:36 +1300

    feat: #23 added mongostore session cookies

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
 const flash = require('connect-flash');
+const MongoStore = require('connect-mongo');
 const passport = require('passport');
 const path = require('path');
 const initialiseDb = require(path.join(__dirname, 'utils', 'database'));
@@ -29,28 +30,32 @@ app.use('/js', express.static(path.join(__dirname, '../node_modules', 'bootstrap
 // Parse incoming request bodies (for form data)
 app.use(express.urlencoded({ extended: true }));
 
-app.use(flash());
-
 // Set up sessions
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    cookie: {
+        maxAge: 6000000 * 60 * 24, // 1 day
+        sameSite: 'lax',
+        secure: false
+    }
 }));
 
 // Initialize Passport and session for Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+    next();
+});
+
+app.use(flash());
 // Define routes
 app.use('/auth', require('./routes/auth'));
 app.use('/courses', require('./routes/courses'));
 
-// Ensure authenticated middleware for protected routes
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect('/login');
-}
 
 // Root route
 app.get('/', (req, res) => {
