850cee8a4da50f40b4a0c31923987e7f40b93ce1
Samuel Richards <richards.m.samuel@gmail.com>
2023-10-04 20:52:37 +1300

    feat: #23 added req to callback for google oauth

--- src/utils/passport-setup.js
+++ src/utils/passport-setup.js
@@ -9,11 +9,12 @@ const User = require(path.join(__dirname, '..', 'models', 'user'));
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
-}, async (accessToken, refreshToken, profile, done) => {
+  callbackURL: "/auth/google/callback",
+  passReqToCallback: true
+}, async (req, accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
-
+    
     if (!user) {
       user = await User.create({
         googleId: profile.id,
@@ -37,7 +38,7 @@ passport.use(new LocalStrategy({
       if (!user) return done(null, false, { message: 'No user found.' });
       const isMatch = await bcrypt.compare(loginPassword, user.password);
       if (!isMatch) return done(null, false, { message: 'Incorrect password.' });
-
+      
       return done(null, user);
     } catch (error) {
       return done(error);
@@ -51,6 +52,10 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
-  done(null, user);
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
 });
